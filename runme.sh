SolidRun/cn913x_build

commit 88ff06593703cadebf8da20f717dda03a0a7e2b0

DPDK: copy l2fwd and l3fwd applications to /usr/bin
Signed-off-by: Alvaro Karsz <alvaro.karsz@solid-run.com>

runme.sh
@@ -401,6 +401,8 @@ e2cp -G 0 -O 0 $ROOTDIR/images/tmp/boot/*.dtb $ROOTDIR/images/tmp/ubuntu-core.ex
 
 # Copy DPDK applications
 e2cp -G 0 -O 0 -p $ROOTDIR/build/dpdk/build/app/dpdk-testpmd $ROOTDIR/images/tmp/ubuntu-core.ext4:usr/bin/
+e2cp -G 0 -O 0 -p $ROOTDIR/build/dpdk/build/examples/dpdk-l2fwd $ROOTDIR/images/tmp/ubuntu-core.ext4:usr/bin/
+e2cp -G 0 -O 0 -p $ROOTDIR/build/dpdk/build/examples/dpdk-l3fwd $ROOTDIR/images/tmp/ubuntu-core.ext4:usr/bin/
 
 # Copy MUSDK
 cd $ROOTDIR/build/musdk-marvell-SDK11.22.07/usr/local/

commit 5d155c2e33c6cdfb9f2910a590b157e4531601be

Resize the output image
Add extra 50B, needed for the DPDK applications for some UBUNTU
versions.

Fixes: 88ff065 ("DPDK: copy l2fwd and l3fwd applications to /usr/bin")
Signed-off-by: Alvaro Karsz <alvaro.karsz@solid-run.com>

runme.sh
@@ -264,7 +264,7 @@ EOF
 	chmod +x overlay/etc/init.d/S99bootstrap-ubuntu.sh
 	make
 	IMG=ubuntu-core.ext4.tmp
-	truncate -s 450M $IMG
+	truncate -s 500M $IMG
 	qemu-system-aarch64 -m 1G -M virt -cpu cortex-a57 -nographic -smp 1 -kernel output/images/Image -append "console=ttyAMA0 ip=dhcp" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -initrd output/images/rootfs.cpio.gz -drive file=$IMG,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -no-reboot
         mv $IMG $ROOTDIR/build/ubuntu-core.ext4
 
@@ -446,8 +446,8 @@ e2cp -G 0 -O 0 $ROOTDIR/build/musdk-marvell-SDK11.22.07/modules/pp2/mv_pp_uio.ko
 cp $ROOTDIR/build/arm-trusted-firmware/build/t9130/release/flash-image.bin $ROOTDIR/images
 cp $ROOTDIR/build/linux/arch/arm64/boot/Image $ROOTDIR/images
 cd $ROOTDIR/
-truncate -s 520M $ROOTDIR/images/tmp/ubuntu-core.img
-parted --script $ROOTDIR/images/tmp/ubuntu-core.img mklabel msdos mkpart primary 64MiB 517MiB
+truncate -s 570M $ROOTDIR/images/tmp/ubuntu-core.img
+parted --script $ROOTDIR/images/tmp/ubuntu-core.img mklabel msdos mkpart primary 64MiB 567MiB
 # Generate the above partuuid 3030303030 which is the 4 characters of '0' in ascii
 echo "0000" | dd of=$ROOTDIR/images/tmp/ubuntu-core.img bs=1 seek=440 conv=notrunc
 dd if=$ROOTDIR/images/tmp/ubuntu-core.ext4 of=$ROOTDIR/images/tmp/ubuntu-core.img bs=1M seek=64 conv=notrunc